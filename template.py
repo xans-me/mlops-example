c01cf7f493a20d8f0b975e65b768d50421163336
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2024-11-05 01:14:43 +0700

    Setup new template

--- template.py
+++ template.py
@@ -1,46 +1,41 @@
 import os
 from pathlib import Path
+import logging
 
-project_name = "us_visa"
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: [%(levelname)s] - %(message)s')
+
+project_name = "cnnClassifier"
 
 list_of_files = [
-    f"{project_name}/__init__.py",
-    f"{project_name}/components/__init__.py",
-    f"{project_name}/components/data_ingestion.py",
-    f"{project_name}/components/data_validation.py",
-    f"{project_name}/components/data_transformation.py",
-    f"{project_name}/components/model_trainer.py",
-    f"{project_name}/components/model_evaluation.py",
-    f"{project_name}/components/model_pusher.py",
-    f"{project_name}/configuration/__init__.py",
-    f"{project_name}/constant/__init__.py",
-    f"{project_name}/entity/__init__.py",
-    f"{project_name}/entity/config_entity.py",
-    f"{project_name}/entity/artifact_entity.py",
-    f"{project_name}/exception/__init__.py",
-    f"{project_name}/logger/__init__.py",
-    f"{project_name}/pipeline/__init__.py",
-    f"{project_name}/pipeline/training_pipeline.py",
-    f"{project_name}/pipeline/prediction_pipeline.py",
-    f"{project_name}/utils/__init__.py",
-    f"{project_name}/utils/main_utils.py",
-    "app.py",
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/constant/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
     "requirements.txt",
-    "Dockerfile",
-    ".dockerignore",
-    "demo.py",
     "setup.py",
-    "config/model.yaml",
-    "config/schema.yaml",
+    "research/trial.ipynb",
+    "templates/index.html",
 ]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
+    
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
+        logging.info(f"Creating empty file: {filepath}")
     else:
-        print(f"file is already exists at: {filepath}")
+        logging.info(f"{filename} already exists in the directory")
